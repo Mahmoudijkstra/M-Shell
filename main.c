8f95db2eb9d709c53c2ac7719451496efeb4b5f7
Ilyes-El-Ouni <i.elouni@cynoia.com>
2024-12-23 00:52:22 +0200

    fix execute commands

--- main.c
+++ main.c
@@ -7,70 +7,74 @@
 
 const int MAX_ARGS = 128;
 
-
+// Read input from the user
 char* get_input() {
-    
     size_t buffer_size = 0;
     char* buffer = NULL;
     getline(&buffer, &buffer_size, stdin);
-    return buffer;
 
+    // Remove trailing newline if it exists
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+    return buffer;
 }
 
+// Parse the input into arguments
 char** parse_input(char* buffer) {
-
     int i = 0;
     char** tokens = (char**) malloc(sizeof(char*) * MAX_ARGS);
-    char* delim=" ";
+    char* delim = " ";
     char* token = strtok(buffer, delim);
-    while(token != NULL) {
+    while (token != NULL) {
         tokens[i] = token;
         token = strtok(NULL, delim);
         i++;
     }
-    tokens[i]=NULL;
+    tokens[i] = NULL;
     return tokens;
-
 }
 
-int execute_command (char** args) {
-
+// Execute the command
+int execute_command(char** args) {
     pid_t pid = fork();
-    if(pid == -1) {
+    if (pid == -1) {
         perror("Fork failed");
         return 1;
     }
     if (pid == 0) {
-        //child process
-        //char *argv[] = {"/bin/ls", "-l", NULL};  
-        //execve(argv[0], argv, NULL);
-        char path[200] ="/bin/";
+        // Child process
+        char path[200] = "/bin/";
         strcat(path, args[0]);
-        args[0]=path;
-        execve(args[0], args, NULL);
-    }
-    else {
-        //parent process
+
+        execve(path, args, NULL);
+        perror("execve failed"); // If execve fails
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        //waitpid(pid, &status, 0);
         wait(NULL);
         printf("Done with execve\n");
     }
     return 1;
 }
 
-int main (void) {
-
+// Main shell loop
+int main(void) {
     int status = 1;
     char* buffer;
     char** args;
+
     while (status) {
-        printf("M-Shell>");
+        printf("M-Shell> ");
         buffer = get_input();
         args = parse_input(buffer);
-        status = execute_command(args);
+        if (args[0] != NULL) { // Ensure command is not empty
+            status = execute_command(args);
+        }
         free(buffer);
         free(args);
     }
-    
     return 0;
-
 }
