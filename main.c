e7090592c2aeb150f7ebf1ba8c178b51309321de
Mahmoudijkstra <188392973+Mahmoudijkstra@users.noreply.github.com>
2024-12-22 17:20:28 +0100

    Add parse_input

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+const int MAX_ARGS = 128;
+
 
 char* get_input() {
     
@@ -13,12 +17,32 @@ char* get_input() {
 
 }
 
+char** parse_input(char* buffer) {
+
+    int i = 0;
+    char** tokens = (char**) malloc(sizeof(char*) * MAX_ARGS);
+    char* delim=" ";
+    char* token = strtok(buffer, delim);
+    while(token != NULL) {
+        tokens[i] = token;
+        token = strtok(NULL, delim);
+        i++;
+    }
+    tokens[i]=NULL;
+    return tokens;
+
+}
+
+
 int main (void) {
 
     char* buffer;
+    char** args;
     printf("M-Shell>");
     buffer = get_input();
+    args = parse_input(buffer);
     free(buffer);
+    free(args);
     return 0;
 
 }
