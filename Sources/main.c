b7ff464e044c3edfb8ad984044a4584dca45583a
Ilyes-El-Ouni <i.elouni@cynoia.com>
2024-12-29 12:18:38 +0200

    update main

--- Sources/main.c
+++ Sources/main.c
@@ -11,7 +11,7 @@ int main (void) {
         buffer = get_input();
         save_to_history(buffer);
         args = parse_input(buffer);
-        if (handle_builtin(args)) continue;
+        if (!handle_builtin(args));
         execute_command(args);
         free_input(buffer, args);
     }
